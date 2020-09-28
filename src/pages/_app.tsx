9f54a6430099c99c34df1f78c1e05852ca1176aa
skoto <0120koto@gmail.com>
2020-09-28 21:37:02 +0900

    :sparkles: Introduce new features.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,8 @@
-import "ress/dist/ress.min.css";
+import "ress";
+import "src/styles/global.scss";
 
-import { AppProps } from "next/app";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return (
-    <>
-      <Component {...pageProps} />
-      <style jsx global>{`
-        body {
-          font-family: 游明朝, "Yu Mincho", YuMincho, "Hiragino Mincho ProN", "Hiragino Mincho Pro", HGS明朝E, メイリオ,
-            Meiryo, serif;
-        }
-        h1,
-        h2,
-        h3 {
-          font-weight: 600;
-        }
-        ul {
-          list-style: none;
-        }
-        .sansSerif {
-          font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
-        }
-      `}</style>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
