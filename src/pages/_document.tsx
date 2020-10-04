5c83205dcbce56784cb7178dae02ae250368b40d
skoto <0120koto@gmail.com>
2020-10-05 01:04:29 +0900

    :globe_with_meridians: Internationalization and localization.

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
