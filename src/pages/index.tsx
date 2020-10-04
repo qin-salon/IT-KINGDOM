87e8c4bf4e6356404148f6760853f047b4d20a00
skoto <0120koto@gmail.com>
2020-10-05 00:55:23 +0900

    :ambulance: Critical hotfix.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ export default function IndexPage(props: Props): JSX.Element {
           name="description"
           content={`チャンネル登録者${props.subscriberCount}人のプログラミングYouTuberによるオンラインサロン。ヤフーやクックパッドなど国内大手IT企業出身の現役ITエンジニアが運営。`}
         />
+        <meta name="robots" content="noindex" />
       </Head>
       <Global>
         <Header />
