38204022610620ca03ce4dddbb9d6e218ad0b06f
skoto <0120koto@gmail.com>
2020-10-11 17:14:27 +0900

    :zap: Improve performance.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withOptimizedImages = require("next-optimized-images");
+const { resolve } = require("path");
+
+module.exports = withOptimizedImages({
+  // next-optimized-images
+  webpack: (config) => {
+    config.resolve.alias["@img"] = resolve(__dirname, "public/img");
+    return config;
+  },
+
+  // Next.js
   reactStrictMode: true,
   typescript: { ignoreDevErrors: true },
   poweredByHeader: false,
-};
+});
--- src/components/message-price-entry/index.tsx
+++ src/components/message-price-entry/index.tsx
@@ -1,3 +1,4 @@
+import Img from "react-optimized-image";
 import { Entry } from "src/components/entry";
 import { Message } from "src/components/message";
 import styles from "src/components/message-price-entry/index.module.scss";
@@ -6,19 +7,13 @@ import { Price } from "src/components/price";
 export function MessagePriceEntry(): JSX.Element {
   return (
     <div className={styles.container}>
-      <img
+      <Img
         className={styles.img}
-        srcSet="img/message-520.png 520w,
-          img/message-768.png 768w,
-          img/message-980.png 980w,
-          img/message-1200.png 1200w,
-          img/message-1680.png 1680w,
-          img/message-2560.png 2560w"
-        src="img/message-original.png"
-        sizes="(max-width:1280px) 100vw, 1280px"
+        src={require("@img/message.png")}
+        sizes={[520, 768, 980, 1200, 1680, 2560]}
         alt="Background"
+        webp
       />
-      {/* <img className={styles.img} src="img/message.png" alt="Background" /> */}
       <Message />
       <Price />
       <Entry />
