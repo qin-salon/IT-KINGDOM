aa136ef272aaa7187d50ced197cf02cce46daba1
skoto <0120koto@gmail.com>
2020-10-12 17:57:56 +0900

    :white_check_mark: Update tests.

--- test/__mocks__/fileMock.js
+++ test/__mocks__/fileMock.js
@@ -1,1 +1,13 @@
+global.IntersectionObserver = class IntersectionObserver {
+  observe() {
+    return null;
+  }
+  disconnect() {
+    return null;
+  }
+  unobserve() {
+    return null;
+  }
+};
+
 module.exports = "test-file-stub";
