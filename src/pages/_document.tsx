2a89506bbd02dd2d4e07d5e64f480c847d765258
skoto <0120koto@gmail.com>
2020-11-28 12:08:51 +0900

    :lipstick: Add favicon.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,16 @@ export default class MyDocument extends Document {
     return initialProps;
   }
   render(): JSX.Element {
+    const faviconPath = "favicon_io";
     return (
       <Html lang="ja">
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href={`${faviconPath}/apple-touch-icon.png`} />
+          <link rel="icon" type="image/png" sizes="32x32" href={`${faviconPath}/favicon-32x32.png`} />
+          <link rel="icon" type="image/png" sizes="16x16" href={`${faviconPath}/favicon-16x16.png`} />
+          <link rel="manifest" href={`${faviconPath}/site.webmanifest`} />
+          <meta name="theme-color" content="#000000" key="themeColor" />
+        </Head>
         <body>
           <Main />
           <NextScript />
