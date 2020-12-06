3502141914f603994057b500aefe82605466125c
skoto <0120koto@gmail.com>
2020-12-06 16:59:15 +0900

    :bento: Update assets.

--- src/components/members/index.tsx
+++ src/components/members/index.tsx
@@ -5,28 +5,28 @@ import styles from "src/components/members/index.module.scss";
 
 const MEMBERS = [
   {
-    src: "/img/members/shimabu.png",
+    src: "/img/members/shimabu.jpg",
     name: "しまぶー",
     rank: "国王",
     introduction:
       "ITエンジニアYouTuber。神戸大学経営学部卒。未経験から独学でプログラミングを勉強し、新卒でヤフーに入社。2019年に株式会社GameHintを創業。",
   },
   {
-    src: "/img/members/matsu.png",
+    src: "/img/members/matsu.jpg",
     name: "まっつん",
     rank: "相国",
     introduction:
       "新卒でヤフーに入社し、決済部署でインフラ・バックエンドを担当。現在はしまぶーと一緒に起業。サロンではバックエンドを見る。",
   },
   {
-    src: "/img/members/ippo.png",
+    src: "/img/members/ippo.jpg",
     name: "いっぽ",
     rank: "左丞相",
     introduction:
       "新卒でクックパッドに入社し現在はITベンチャーに転職。アプリ・フロントエンド・デザインに秀でており、サロンでもその分野を担当。",
   },
   {
-    src: "/img/members/yasu.png",
+    src: "/img/members/yasu.jpg",
     name: "やっさん",
     rank: "右丞相",
     introduction:
