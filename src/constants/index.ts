d8fe68f2d33b6df3374812a464398511ab6fffe8
skoto <0120koto@gmail.com>
2020-12-08 18:42:23 +0900

    :speech_balloon: Update text and literals.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const QIN_URL = "https://qin.salon/signup";
 export const CONTACT_URL =
   "https://docs.google.com/forms/d/e/1FAIpQLSf7lYhGHmnl2KTPXsArv5KCoZvlwX7pMF7EZrDXSgWQWnJDhw/viewform";
-export const MAIL_ADDRESS = "dev@qin.salon";
+export const MAIL_ADDRESS = "support@qin.salon";
